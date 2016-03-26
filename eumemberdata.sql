f08002cc40d5affba0dcf05107fbf638ac0d81a4
Jere Käpyaho <jere@coniferproductions.com>
2016-03-26 22:36:41 +0200

    SQL statements to create the SQLite database

---
+++ eumemberdata.sql
@@ -0,0 +1,79 @@
+/*
+ * 'country_code' - ISO 3166 two-letter code, like 'FI'
+ * 'capital' - foreign key to the 'city' table
+ * 'area' - the area of the country in square kilometers
+ * 'population' - the country's population count or estimate
+ * 'population_year' - the year of the population count
+ * 'currency_code' - the currency code of the country's currency (can be used to
+ * retrieve the currency name(s) from the 'currency_name' table)
+ */
+CREATE TABLE IF NOT EXISTS country (
+    country_code TEXT PRIMARY KEY NOT NULL,
+    capital TEXT NOT NULL REFERENCES city(city_id),
+    area INTEGER,
+    population FLOAT,
+    population_year INTEGER,
+    currency_code TEXT NOT NULL
+);
+
+/*
+ * 'country_code' - ISO 3166 two-letter code, like 'FI'
+ * 'language_code' - the ISO 639 two-letter code, like 'en'
+ * 'name' - the name of the country in the language indicated by 'language_code'
+ */
+CREATE TABLE IF NOT EXISTS country_name (
+    country_code TEXT NOT NULL,
+    language_code TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+/*
+ * 'city_id' - the unique ID of the city as a string (its English name folded into lowercase)
+ * latitude, longitude - the location of some central place in the city (from Wikipedia)
+ */
+CREATE TABLE IF NOT EXISTS city (
+    city_id TEXT PRIMARY KEY NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+);
+
+/*
+ * city_id - foreign key to the city table
+ * 'language_code' - the ISO 639 two-letter code, like 'en'.
+ * 'name' - the name of the city in the language indicated by 'language_code'
+ */
+CREATE TABLE IF NOT EXISTS city_name (
+    city_id INTEGER NOT NULL REFERENCES city(city_id),
+    language_code TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS currency (
+    currency_code TEXT PRIMARY KEY NOT NULL,
+    currency_symbol TEXT
+);
+
+/*
+ * 'currency_code' - the ISO 4217 three-letter code, like 'EUR'.
+ * 'language_code' - the ISO 639 two-letter code, like 'en'.
+ * 'name' - the name of the currency in the language indicated by the
+ * 'language_code' column.
+ */
+CREATE TABLE IF NOT EXISTS currency_name(
+    currency_code TEXT NOT NULL,
+    language_code TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+/*
+ * The years when a country has joined the union, the eurozone, or the Schengen treaty.
+ * If the country is not a member of the eurozone or Schengen, the corresponding column value is NULL,
+ * otherwise the value is the year in the Gregorian calendar. Obviously the year of joining the
+ * union cannot be NULL, since there are only member countries in this data.
+ */
+CREATE TABLE IF NOT EXISTS joined(
+    country_code TEXT NOT NULL,
+    union_year INTEGER NOT NULL,
+    eurozone_year INTEGER,
+    schengen_year INTEGER
+);
